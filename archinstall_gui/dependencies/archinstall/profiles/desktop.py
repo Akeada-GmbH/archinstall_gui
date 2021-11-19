12ddd7d4fe5676983d32877b6d5557fb57bcfce7
jarhat <jarhat@protonmail.com>
2021-11-19 22:06:52 +0100

    Updated archinstaller dependency

--- archinstall_gui/dependencies/archinstall/profiles/desktop.py
+++ archinstall_gui/dependencies/archinstall/profiles/desktop.py
@@ -1,25 +1,83 @@
+# A desktop environment selector.
+
 import archinstall
 
-arguments = {
-	'keyboard_layout' : 'sv-latin1',
-	"editor" : "nano",
-	"mediaplayer" : "lollypop gstreamer gst-plugins-good gnome-keyring",
-	"filebrowser" : "nemo gpicview-gtk3",
-	"webbrowser" : "chromium",
-	"window_manager" : "awesome",
-	"virtulization" : "qemu ovmf",
-	"utils" : "openssh sshfs git htop pkgfile scrot dhclient wget smbclient cifs-utils libu2f-host",
-	"audio" : "pulseaudio pulseaudio-alsa pavucontrol"
-}
+is_top_level_profile = True
 
-installation.add_additional_packages("{webbrowser} {utils} {mediaplayer} {window_manager} {virtulization} {filebrowser} {editor}".format(**arguments))
+__description__ = 'Provides a selection of desktop environments and tiling window managers, e.g. gnome, kde, sway'
 
-with open(f'{installation.mountpoint}/etc/X11/xinit/xinitrc', 'a') as X11:
-	X11.write('setxkbmap se\n')
+# New way of defining packages for a profile, which is iterable and can be used out side
+# of the profile to get a list of "what packages will be installed".
+__packages__ = [
+	'nano',
+	'vim',
+	'openssh',
+	'htop',
+	'wget',
+	'iwd',
+	'wireless_tools',
+	'wpa_supplicant',
+	'smartmontools',
+	'xdg-utils',
+]
 
-with open(f'{installation.mountpoint}/etc/vconsole.conf', 'a') as vconsole:
-	vconsole.write('KEYMAP={keyboard_layout}\n'.format(**arguments))
-	vconsole.write('FONT=lat9w-16\n')
+__supported__ = [
+	'gnome',
+	'kde',
+	'awesome',
+	'sway',
+	'cinnamon',
+	'xfce4',
+	'lxqt',
+	'i3',
+	'budgie',
+	'mate',
+	'deepin',
+	'enlightenment',
+]
 
-awesome = archinstall.Application(installation, 'awesome')
-awesome.install()
+
+def _prep_function(*args, **kwargs):
+	"""
+	Magic function called by the importing installer
+	before continuing any further. It also avoids executing any
+	other code in this stage. So it's a safe way to ask the user
+	for more input before any other installer steps start.
+	"""
+
+	desktop = archinstall.generic_select(__supported__, 'Select your desired desktop environment: ', allow_empty_input=False, sort=True)
+
+	# Temporarily store the selected desktop profile
+	# in a session-safe location, since this module will get reloaded
+	# the next time it gets executed.
+	if not archinstall.storage.get('_desktop_profile', None):
+		archinstall.storage['_desktop_profile'] = desktop
+	if not archinstall.arguments.get('desktop-environment', None):
+		archinstall.arguments['desktop-environment'] = desktop
+	profile = archinstall.Profile(None, desktop)
+	# Loading the instructions with a custom namespace, ensures that a __name__ comparison is never triggered.
+	with profile.load_instructions(namespace=f"{desktop}.py") as imported:
+		if hasattr(imported, '_prep_function'):
+			return imported._prep_function()
+		else:
+			print(f"Deprecated (??): {desktop} profile has no _prep_function() anymore")
+
+
+if __name__ == 'desktop':
+	"""
+	This "profile" is a meta-profile.
+	There are no desktop-specific steps, it simply routes
+	the installer to whichever desktop environment/window manager was chosen.
+
+	Maybe in the future, a network manager or similar things *could* be added here.
+	We should honor that Arch Linux does not officially endorse a desktop-setup, nor is
+	it trying to be a turn-key desktop distribution.
+
+	There are plenty of desktop-turn-key-solutions based on Arch Linux,
+	this is therefore just a helper to get started
+	"""
+
+	# Install common packages for all desktop environments
+	archinstall.storage['installation_session'].add_additional_packages(__packages__)
+
+	archinstall.storage['installation_session'].install_profile(archinstall.storage['_desktop_profile'])
